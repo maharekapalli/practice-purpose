e980cdb940b3bb8f2b3bbedcf864aa447de16325
maharekapalli <88081912+maharekapalli@users.noreply.github.com>
2021-08-04 16:44:01 +0530

    added new line

--- sum.java
+++ sum.java
@@ -1,6 +1,7 @@
 #include<java.lang.com>
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello world");  
+     System.out.println("Hello world"); 
+     System.out.println("practicing for job purpose");
     }  
 }  
