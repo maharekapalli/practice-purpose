1f7bd9fef751424794978c1048a85ac54320e1c2
maharekapalli <88081912+maharekapalli@users.noreply.github.com>
2021-08-04 16:36:32 +0530

    change done

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 #include<java.lang.com>
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello world");  
     }  
 }  
